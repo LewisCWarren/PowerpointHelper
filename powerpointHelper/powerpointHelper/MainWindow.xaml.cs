1659f71cc72df6c0925f05f663bd36b8abd67ecc
Lewis Warren <57116148+LewisCWarren@users.noreply.github.com>
2021-01-06 17:35:28 -0800

    Add initial files and GUI

---
+++ powerpointHelper/powerpointHelper/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace powerpointHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string value { get; set; }
+        public string body { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            //textBlock.Text = userTitle.Text;
+            textBlock.Text = richText.Selection.ToString();
+
+        }
+    }
+}
